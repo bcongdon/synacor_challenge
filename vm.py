bc4f4d7a5c9c5375ecfd85b168fff8c9c86051ba
Benjamin Congdon <bencongdon96@gmail.com>
2016-12-10 15:01:50 -0600

    Rewrite to output decompiled bytecode

--- vm.py
+++ vm.py
@@ -100,6 +100,31 @@ class OperatorUnit:
             20: self.op_in,
             21: self.noop,
         }
+
+        self.opcodes = {
+            0: 'halt',
+            1: 'set',
+            2: 'push',
+            3: 'pop',
+            4: 'eq',
+            5: 'gt',
+            6: 'jmp',
+            7: 'jt',
+            8: 'jf',
+            9: 'add',
+            10: 'mul',
+            11: 'mod',
+            12: 'and',
+            13: 'or',
+            14: 'not',
+            15: 'rmem',
+            16: 'wmem',
+            17: 'call',
+            18: 'ret',
+            19: 'out',
+            20: 'in',
+            21: 'noop',
+        }
         self.input_buf = ''
 
     def next_values(self, i):
@@ -111,55 +136,60 @@ class OperatorUnit:
     def noop(self):
         pass
 
-    def op_out(self):
-        a = self.next_value()
+    def op_out(self, a):
+        a = self.vm.load_val(a)
         print(chr(a), end="")
 
-    def op_in(self):
-        a = self.vm.next_byte()
+    def op_in(self, a):
         if not self.input_buf:
-            self.input_buf = raw_input('>>') + '\n'
+            raw, done = raw_input('>>'), False
+            while not done:
+                if raw == 'pc':
+                    print("PC: " + str(self.vm.pc))
+                if raw == 'cd':
+                    self.vm.core_dump()
+                else:
+                    done = True
+                    self.input_buf = raw + '\n'
+                raw = raw_input('>>')
         self.vm.store_reg(a, ord(self.input_buf[0]))
         self.input_buf = self.input_buf[1:]
 
-    def op_operator(self, operation):
-        a = self.vm.next_byte()
-        b, c = self.next_values(2)
+    def op_operator(self, operation, a, b, c):
+        b, c = map(self.vm.load_val, (b, c))
         res = int(operation(b, c)) % 32768
         self.vm.store_reg(a, res)
 
-    def op_not(self):
-        a = self.vm.next_byte()
-        b = self.next_value()
+    def op_not(self, a, b):
+        b = self.vm.load_val(b)
         res = ~b & 0x7FFF
         self.vm.store_reg(a, res)
 
-    def op_jmp(self):
-        a = self.next_value()
+    def op_jmp(self, a):
+        a = self.vm.load_val(a)
         self.vm.pc = a
 
-    def op_jt(self):
-        a, b = self.next_values(2)
+    def op_jt(self, a, b):
+        a, b = map(self.vm.load_val, (a, b))
         if a != 0:
             self.vm.pc = b
 
-    def op_jf(self):
-        a, b = self.next_values(2)
+    def op_jf(self, a, b):
+        a, b = map(self.vm.load_val, (a, b))
         if a == 0:
             self.vm.pc = b
 
-    def op_call(self):
-        self.vm.stack_push(self.vm.pc + 1)
-        a = self.next_value()
+    def op_call(self, a):
+        a = self.vm.load_val(a)
+        self.vm.stack_push(self.vm.pc)
         self.vm.pc = a
 
     def op_ret(self):
         new_pc = self.vm.stack_pop()
         self.vm.pc = new_pc
 
-    def op_set(self):
-        a = self.vm.next_byte()
-        b = self.next_value()
+    def op_set(self, a, b):
+        b = self.vm.load_val(b)
         self.vm.store_reg(a, b)
 
     def op_halt(self):
@@ -167,23 +197,20 @@ class OperatorUnit:
         self.vm.core_dump()
         sys.exit()
 
-    def op_push(self):
-        a = self.next_value()
+    def op_push(self, a):
+        a = self.vm.load_val(a)
         self.vm.stack_push(a)
 
-    def op_pop(self):
-        a = self.vm.next_byte()
+    def op_pop(self, a):
         res = self.vm.stack_pop()
         self.vm.store_reg(a, res)
 
-    def op_wmem(self):
-        a = self.next_value()
-        b = self.next_value()
+    def op_wmem(self, a, b):
+        a, b = map(self.vm.load_val, (a, b))
         self.vm.write_mem(a, b)
 
-    def op_rmem(self):
-        a = self.vm.next_byte()
-        b = self.next_value()
+    def op_rmem(self, a, b):
+        b = self.vm.load_val(b)
         val = self.vm.read_mem(b)
         self.vm.store_reg(a, val)
 
@@ -194,22 +221,39 @@ def num_args(func):
     else:
         return len(inspect.getargspec(func).args)
 
+
+def format_val(val):
+    if val <= 32767:
+        return str(val)
+    else:
+        return '$%s' % (val - 32768)
+
 if __name__ == '__main__':
     virtual_machine = VirtualMachine('challenge.bin')
     op_unit = OperatorUnit(virtual_machine)
     cycle_count = 0
 
-
     def on_close():
         print("Cycle count: %s" % cycle_count)
 
     atexit.register(on_close)
-    while True:
-        cycle_count += 1
-        op = virtual_machine.next_byte()
-        if op in op_unit.op_functions:
-            op_unit.op_functions[op]()
-        else:
-            print("Don't have operation for opcode: %s" % op)
-            virtual_machine.core_dump()
-            break
+    with open('bytecode.asm', 'w+') as f:
+        while True:
+            cycle_count += 1
+            op = virtual_machine.next_byte()
+            if op in op_unit.op_functions:
+                func = op_unit.op_functions[op]
+                args = [virtual_machine.next_byte()
+                        for _ in range(num_args(func) - 1)]
+                args_out = map(format_val, args)
+                if op == 19 and not args_out[0][0] == '$':
+                    args_out[0] = "'%s'" % chr(int(args_out[0])).encode('string-escape')
+                out = op_unit.opcodes[op] + '\t'
+                out += ', '.join(args_out)
+                out += '\n'
+                f.write(out)
+                func(*args)
+            else:
+                print("Don't have operation for opcode: %s" % op)
+                virtual_machine.core_dump()
+                break
