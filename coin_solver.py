3bd4eeb1a5977aac312a997396beac791c95abd5
Benjamin Congdon <bencongdon96@gmail.com>
2016-12-10 15:01:31 -0600

    Coin game solver

---
+++ coin_solver.py
@@ -0,0 +1,20 @@
+from itertools import permutations
+
+
+def check_solution(arr):
+    a, b, c, d, e = arr
+    return a + b * c ** 2 + d **3 - e == 399
+
+if __name__ == '__main__':
+    coins = {
+        2: 'red',
+        3: 'corroded',
+        5: 'shiny',
+        7: 'concave',
+        9: 'blue'
+    }
+
+    sol = next(i for i in permutations(coins.keys(), len(coins))
+               if check_solution(i))
+    print('Correct order: ' + ', '.join([coins[i] for i in sol]))
+    print('({} + {}*{}^2 + {}^3 - {} = 399)'.format(*sol))
